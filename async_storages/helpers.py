03ab2f8204757600c321a5fcad49e6462d637907
alex.oleshkevich <alex.oleshkevich@gmail.com>
2024-08-26 12:40:37 +0200

    improve docs

--- async_storages/helpers.py
+++ async_storages/helpers.py
@@ -21,7 +21,20 @@ def generate_file_path(
     destination: str,
     extra_tokens: typing.Mapping[str, typing.Any] | None = None,
 ) -> str:
-    """Generate file path interpolation tokens in destination."""
+    """
+    Generate file path interpolation tokens in destination.
+
+    Built-in interpolation tokens:
+    - {random} - 8 random hex digits
+    - {uuid} - random UUID
+    - {date} - current date in ISO format
+    - {datetime} - current datetime in ISO format
+    - {time} - current time in ISO format
+    - {timestamp} - current timestamp
+    - {file_name} - sanitized file name
+    - {name} - file name without extension
+    - {extension} - file extension without dot
+    """
     now = _get_now()
     return destination.format(
         # randomization
